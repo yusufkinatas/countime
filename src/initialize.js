bdb61ee86326a3d76072e79b564652c9ab2534fb
Yusuf Kınataş <yusufkinatas@gmail.com>
2020-04-22 22:37:48 +0300

    added env variables added placeholder for .env file

--- src/initialize.js
+++ src/initialize.js
@@ -1,5 +1,25 @@
 import firebase from 'firebase/app';
 
-const firebaseConfig = require("./config.json")
+console.log('ENV', process.env);
+
+const {
+  REACT_APP_apiKey: apiKey,
+  REACT_APP_authDomain: authDomain,
+  REACT_APP_databaseURL: databaseURL,
+  REACT_APP_projectId: projectId,
+  REACT_APP_storageBucket: storageBucket,
+  REACT_APP_messagingSenderId: messagingSenderId,
+  REACT_APP_appId: appId,
+} = process.env;
+
+const firebaseConfig = {
+  apiKey,
+  authDomain,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+};
 
 firebase.initializeApp(firebaseConfig);
