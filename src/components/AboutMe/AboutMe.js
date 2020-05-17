a802f4475196d3cbb992b1854688bb112c971e8f
Yusuf Kınataş <yusufkinatas@gmail.com>
2020-05-17 14:27:34 +0300

    improved aboutMe section

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import email from 'assets/email.png';
+import linkedin from 'assets/linkedin.png';
+import github from 'assets/github.png';
 
 import './AboutMe.css';
 
@@ -15,12 +18,33 @@ function AboutMe() {
       <div onClick={toggle} className={'aboutme-handle' + (visible ? ' visible' : '')}>
         About Me
       </div>
-      <div className={'aboutme-body' + (visible ? ' visible' : '')}>
-        <p>This is contentt</p>
-        <p>This is contentt</p>
-        <p>This is contentt</p>
-        <p>This is contentt</p>
-        <p>This is contentt</p>
+      <div className={'aboutme-body ' + (visible ? ' visible' : '')}>
+        <p>
+          Hi, I am Yusuf. A software developer from Turkey. I'm developing software for different
+          platforms (mobile, desktop, web) since 2014.
+        </p>
+        <p>
+          My favorite part of the software development is to create ease in other people's lives.
+        </p>
+        <p>
+          If you want to contact with me, hire me, see my other projects or just chat while having a
+          cup of coffee, you can use links below.
+        </p>
+        <div className="filler" />
+        <div className="social-buttons">
+          <a className="email" href="mailto:yusufkinatas@gmail.com" target="_blank">
+            <img src={email} />
+            <span>E-Mail</span>
+          </a>
+          <a className="linkedin" href="https://www.linkedin.com/in/yusuf-kinatas/" target="_blank">
+            <img src={linkedin} />
+            <span>Linkedin</span>
+          </a>
+          <a className="github" href="https://github.com/yusufkinatas" target="_blank">
+            <img src={github} />
+            <span>Github</span>
+          </a>
+        </div>
       </div>
     </React.Fragment>
   );
