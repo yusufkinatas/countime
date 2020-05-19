317f0f3b8c75dbef95a867cd4b9f7e6ed2f57259
Yusuf Kınataş <yusufkinatas@gmail.com>
2020-05-19 10:27:51 +0300

    AboutMe css improvements

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -19,43 +19,45 @@ function AboutMe() {
         About Me
       </div>
       <div className={'aboutme-body ' + (visible ? ' visible' : '')}>
-        <p>
-          Hi, I am Yusuf. A software developer from Turkey. I'm developing software for different
-          platforms (mobile, desktop, web) since 2014.
-        </p>
-        <p>
-          My favorite part of the software development is to create ease in other people's lives.
-        </p>
-        <p>
-          If you want to contact with me, hire me, see my other projects or just chat while having a
-          cup of coffee, you can use links below.
-        </p>
-        <div className="filler" />
-        <div className="social-buttons">
-          <a
-            className="email"
-            href="mailto:yusufkinatas@gmail.com"
-            target="_blank"
-            rel="noopener noreferrer">
-            <img alt="email" src={email} />
-            <span>E-Mail</span>
-          </a>
-          <a
-            className="linkedin"
-            href="https://www.linkedin.com/in/yusuf-kinatas/"
-            target="_blank"
-            rel="noopener noreferrer">
-            <img alt="linkedin" src={linkedin} />
-            <span>Linkedin</span>
-          </a>
-          <a
-            className="github"
-            href="https://github.com/yusufkinatas"
-            target="_blank"
-            rel="noopener noreferrer">
-            <img alt="github" src={github} />
-            <span>Github</span>
-          </a>
+        <div className="aboutme-content">
+          <p>
+            Hi, I am Yusuf. A software developer from Turkey. I'm developing software for different
+            platforms (mobile, desktop, web) since 2014.
+          </p>
+          <p>
+            My favorite part of the software development is to create ease in other people's lives.
+          </p>
+          <p>
+            If you want to contact with me, hire me, see my other projects or just chat while having
+            a cup of coffee, you can use links below.
+          </p>
+          <div className="filler" />
+          <div className="social-buttons">
+            <a
+              className="email"
+              href="mailto:yusufkinatas@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer">
+              <img alt="email" src={email} />
+              <span>E-Mail</span>
+            </a>
+            <a
+              className="linkedin"
+              href="https://www.linkedin.com/in/yusuf-kinatas/"
+              target="_blank"
+              rel="noopener noreferrer">
+              <img alt="linkedin" src={linkedin} />
+              <span>Linkedin</span>
+            </a>
+            <a
+              className="github"
+              href="https://github.com/yusufkinatas"
+              target="_blank"
+              rel="noopener noreferrer">
+              <img alt="github" src={github} />
+              <span>Github</span>
+            </a>
+          </div>
         </div>
       </div>
     </React.Fragment>
